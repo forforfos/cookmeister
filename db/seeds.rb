93416f7639dd4ff17901ca03731f8ce192a6f55f
Ioannis-Alexandros Papaefthymiou <alexandros.papaefthymiou@etraveligroup.com>
2022-05-10 17:25:34 +0300

    Add provided recipes-en.json file and use it to seed the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'json'
+
+recipes = JSON.parse(File.read("#{Rails.root}/db/recipes-en.json"))
+
+recipes.each do |recipe|
+  Recipe.create(recipe)
+end
