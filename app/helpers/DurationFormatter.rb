84f7809793ffaf86ab9608b80305ccb584887eb8
Ioannis-Alexandros Papaefthymiou <alexandros.papaefthymiou@etraveligroup.com>
2022-05-14 18:23:05 +0300

    Create helper module to format time durations for cook_time and prep_time

---
+++ app/helpers/DurationFormatter.rb
@@ -0,0 +1,26 @@
+module DurationFormatter
+  def hours_literal(hours)
+    return 'hours' if hours > 1
+
+    'hour'
+  end
+  module_function :hours_literal
+
+  def minutes_literal(minutes)
+    return 'minutes' if minutes != 1
+
+    'minute'
+  end
+  module_function :minutes_literal
+
+  def format_by_minutes(minutes)
+    hours = minutes / 60
+    minutes = minutes.modulo(60)
+
+    hours_text = hours.nonzero? && "#{hours} #{hours_literal(hours)},"
+    minutes_text = "#{minutes} #{minutes_literal(minutes)}"
+
+    "#{hours_text} #{minutes_text}"
+  end
+  module_function :format_by_minutes
+end
